25322dcef1f3590080d55595d2b60c7c86024580
Makikotakemori <t_makiko_1996@yahoo.co.jp>
2021-12-02 09:02:30 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
   
+  get 'relationships/followings'
+  get 'relationships/followers'
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   
   devise_for :users
   
   resources :books do
-   resources :book_comments, only:[:create, :destroy]
-   resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   
-  resources :users,only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
  
   
 end
